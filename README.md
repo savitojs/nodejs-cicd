d7b2d3f32337efdc71573f9429da73b6e2ef50ea
Savitoj Singh <savsingh@redhat.com>
2017-05-05 15:55:43 +0530

    Initial code

---
+++ README.md
@@ -0,0 +1,3 @@
+# Test app for test 
+
+This is a quick and dirty test node.js app cobbled together for the purposes of demonstrating a basic CI/CD workflow.
---
+++ app.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var express = require('express'),
+    app = express();
+
+app.set('views', 'views');
+app.set('view engine', 'jade');
+
+app.get('/', function(req, res) {
+    res.render('home', {
+  });
+});
+
+app.listen(8080);
+module.exports.getApp = app;
